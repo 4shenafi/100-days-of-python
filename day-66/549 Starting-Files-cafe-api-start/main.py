2deb26ef749b962724aaa04fcbd68d31f53e7401
4shenafi <apawlos0@gmail.com>
2025-02-23 10:27:29 +0300

    Implement user registration and login functionality with flash messages and user session management

--- day-66/549 Starting-Files-cafe-api-start/main.py
+++ day-66/549 Starting-Files-cafe-api-start/main.py
@@ -31,67 +31,30 @@ def home():
     return render_template("index.html")
 
 ## HTTP GET - Read Record
-@app.route('/random' , methods=['GET'])
+@app.route('/random')
 def get_random_cafe():
     cafe = Cafe.query.order_by(db.func.random()).first()
     return jsonify(model_to_dict(cafe))
 
-@app.route('/all', methods=['GET'])
+@app.route('/all')
 def get_all_cafes():
     cafes = Cafe.query.all()
     return jsonify([model_to_dict(cafe) for cafe in cafes])
 
 @app.route('/search')
 def search():
-    query_location = request.args.get('loc')
-    cafe = Cafe.query.filter_by(location=query_location).first()
+    location = request.args.get('loc')
+    cafe = Cafe.query.filter_by(location=location).first()
     if cafe:
         return jsonify(model_to_dict(cafe))
     else:
         return jsonify({"error": "Cafe not found"}), 404
 
 ## HTTP POST - Create Record
-@app.route('/add', methods=['POST'])
-def add_cafe():
-    new_cafe = Cafe(
-        name=request.form.get('name'),
-        map_url=request.form.get('map_url'),
-        img_url=request.form.get('img_url'),
-        location=request.form.get('location'),
-        seats=request.form.get('seats'),
-        has_toilet=bool(request.form.get('has_toilet')),
-        has_wifi=bool(request.form.get('has_wifi')),
-        has_sockets=bool(request.form.get('has_sockets')),
-        can_take_calls=bool(request.form.get('can_take_calls')),
-        coffee_price=request.form.get('coffee_price')
-    )
-    db.session.add(new_cafe)
-    db.session.commit()
-    return jsonify(response={"success": "Successfully added the new cafe."})
+
 ## HTTP PUT/PATCH - Update Record
-@app.route('/update-price/<int:cafe_id>', methods=['PATCH'])
-def update_price(cafe_id):
-    cafe = Cafe.query.get(cafe_id)
-    if cafe:
-        cafe.coffee_price = request.args.get('new_price')
-        db.session.commit()
-        return jsonify(response={"success": "Successfully updated the price."})
-    else:
-        return jsonify({"error": "Cafe not found"})
 
 ## HTTP DELETE - Delete Record
-@app.route('/report-closed/<int:cafe_id>', methods=['DELETE'])
-def delete_cafe(cafe_id):
-    api_key = request.args.get('api-key')
-    if api_key != "secret":
-        return jsonify({"error": "Sorry, that's not allowed. Make sure you have the correct api-key."}), 403
-    cafe = Cafe.query.get(cafe_id)
-    if cafe:
-        db.session.delete(cafe)
-        db.session.commit()
-        return jsonify(response={"success": "Successfully deleted the cafe."})
-    else:
-        return jsonify({"error": "Cafe not found"})
-        
+
 if __name__ == '__main__':
     app.run(debug=True)
--- day-68/567 Starting-Files-flask-auth-start/main.py
+++ day-68/567 Starting-Files-flask-auth-start/main.py
@@ -10,6 +10,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 ##CREATE TABLE IN DB
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,29 +32,71 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=["POST", "GET"])
 def register():
-    return render_template("register.html")
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        name = request.form.get("name")
+
+        #If user's email already exists
+        if User.query.filter_by(email=email).first():
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+        
+
+        new_user = User(
+            email=email,
+            password=generate_password_hash(password, method='pbkdf2:sha256', salt_length=8),
+            name=name
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        login_user(new_user)
+        return redirect(url_for('secrets', name=new_user.name))
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
 
-@app.route('/login')
+@app.route('/login', methods=["POST", "GET"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+
+        #Email doesn't exist
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+        #Email exists but password is wrong
+        elif not check_password_hash(user.password, password):
+            flash("Password incorrect, please try again.")
+            return redirect(url_for('login'))
+        else:
+            login_user(user)
+            return redirect(url_for('secrets', name=user.name))
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    name = request.args.get('name')
+    return render_template("secrets.html", name=name, logged_in=True)
 
 
 @app.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/download')
+@login_required
 def download():
-    pass
+    return send_from_directory('static', path="files/cheat_sheet.pdf", as_attachment=True)
 
 
 if __name__ == "__main__":
